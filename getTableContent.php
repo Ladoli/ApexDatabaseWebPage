b128ff0b8bd93daa702f2d226884fb04713cf94c
Angelo Villadolid <villadolidangeloryan@gmail.com>
2017-11-27 14:02:15 -0800

    Add files via upload

--- getTableContent.php
+++ getTableContent.php
@@ -1,20 +1,21 @@
 <?php
 		$table =$_POST['table'];
-			
+		$header =$_POST['headers'];
+		$headernames = explode(", ",$header[sizeof($header)]);		
 			$link = mysqli_connect("localhost", "root", "", "demo2300");
 			if($link)
 			{
-				$headernames = "";
-
-				
+				$content = "";
+				$rowfetcher ="";
+				$count = 0;
 				$sql = "SELECT * FROM " . $table;
 				$result = mysqli_query($link, $sql);
 				while($row = mysqli_fetch_assoc($result))
 				{
-				$headernames .=  $row["Ingredientid"].", ".$row["Unit"].", ".$row["IngredientName"].", ".$row["Cost"]."^";
+				$content .=  $row[$headernames[0]].", ".$row[$headernames[1]].", ".$row[$headernames[2]].", ".$row[$headernames[3]]."^";
 				}
 				
-				echo $headernames;
+				echo $content;
 				
 			}
 			
